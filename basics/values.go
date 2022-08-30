53cdd607949469e5724b5186e59be2ed151038d2
IP <shubhamt619@gmail.com>
2022-08-31 02:37:54 +0530

    added values example

---
+++ basics/values.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Welcome to the values Go Program")
+	fmt.Println("Let's explore the values one by one")
+	fmt.Println("This is string concatenation with", 1, "number in it")
+	fmt.Println("This is string concatenation with expression", 12+3)
+}
